e77bf28ee7f5695022bc4f68e35599ec08eff2a0
giovani <inf_giovani@hotmail.com>
2024-03-05 20:29:34 -0300

    atualizacao

--- lib/pages/lista_page_tarefas.dart
+++ lib/pages/lista_page_tarefas.dart
@@ -1,5 +1,8 @@
 
+import 'dart:js';
+
 import 'package:flutter/material.dart';
+import 'package:gerenciador_tareas/model/tarefa.dart';
 
 class ListaTarefaPage extends StatefulWidget{
 
@@ -9,11 +12,15 @@ class ListaTarefaPage extends StatefulWidget{
 
 class _ListaTarefaPageState extends State<ListaTarefaPage>{
 
+  final _tarefas = <Tarefa> [
+    Tarefa(id: 1, descricao: 'Fazer as tarefas', prazo: DateTime.now().add(Duration(days: 5))),
+  ];
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
       appBar: _criarAppBar(),
-      body: Container(),
+      body: _criarBody(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Icon(Icons.add),
@@ -23,16 +30,39 @@ class _ListaTarefaPageState extends State<ListaTarefaPage>{
   }
 
   AppBar _criarAppBar(){
+    backgroundColor: Theme.of(context).colorScheme.primaryContainer;
     return AppBar(
-      backgroundColor: Theme.of(context).colorScheme.primaryContainer,
       title: Text('Tarefas'),
       centerTitle: false,
       actions: [
         IconButton(
             onPressed: () {},
-            icon: Icon(Icons.filter_list),
+            icon: const Icon(Icons.filter_list),
         )
       ],
+    );
+  }
+
+  Widget _criarBody(){
+
+    if(_tarefas.isEmpty){
+      return  const Center(
+        child: Text('Tudo certo por aqui!!!',
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      );
+    }
+
+    return ListView.separated(
+        itemBuilder: (BuildContext context, int index){
+          final tarefa = _tarefas[index];
+          return ListTile(
+            title: Text('${tarefa.id} - ${tarefa.descricao}'),
+            subtitle: Text(tarefa.prazoFormatado),
+    );
+    },
+        separatorBuilder: (BuildContext context, int index) => Divider(),
+        itemCount: _tarefas.length,
     );
   }
 }
