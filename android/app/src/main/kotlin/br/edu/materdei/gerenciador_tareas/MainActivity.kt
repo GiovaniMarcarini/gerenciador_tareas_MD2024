165c0ce5df6762b9b569493744d3faad5bc4946b
giovani <inf_giovani@hotmail.com>
2024-03-05 19:06:54 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/br/edu/materdei/gerenciador_tareas/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.edu.materdei.gerenciador_tareas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:gerenciador_tareas/pages/lista_page_tarefas.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Gerenciador Tarefas',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: ListaTarefaPage(),
+    );
+  }
+}
+
